37fc6ba284420689a4039c367e7eb3f281a7db44
fishmoun <2320836748@qq.com>
2023-06-07 14:43:33 +0800

    更新了模板

---
+++ src/test/java/com/hhu/recommend_backend/RecommendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhu.recommend_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
