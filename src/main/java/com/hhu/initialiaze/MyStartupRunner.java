7a5e8e2f40d20ece02185da34f890f435bd17a01
fishmoun <2320836748@qq.com>
2023-06-24 18:23:22 +0800

    svd更新

--- src/main/java/com/hhu/initialiaze/MyStartupRunner.java
+++ src/main/java/com/hhu/initialiaze/MyStartupRunner.java
@@ -2,6 +2,8 @@ package com.hhu.initialiaze;
 
 import com.hhu.recommender.ItemBasedRecommenderService;
 import com.hhu.recommender.SVDRecommenderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyStartupRunner implements ApplicationRunner {
-
+    private static final Logger logger = LoggerFactory.getLogger(MyStartupRunner.class);
     @Autowired
     private ItemBasedRecommenderService recommenderService; // 替换成你要调用方法的组件
 
@@ -19,6 +21,7 @@ public class MyStartupRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         // 在这里调用你要执行的方法
         svdRecommenderService.initialize();
+        logger.info("处理成功！");
     }
 
 }
